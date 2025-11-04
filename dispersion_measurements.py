d17bfa19dcd3f316ba8d8fbe37aa44f342cd02df
Gabriel Ángel Zárate Domínguez <gabriel.zarate5909@alumnos.udg.mx>
2025-11-04 11:22:26 -0600

    * Refactoring dispersion_measurements

--- dispersion_measurements.py
+++ dispersion_measurements.py
@@ -12,12 +12,13 @@ number_total_elements = len(data);
 average = sum(data) / len(data)
 variance_of_sample = (sum((x - average) ** 2 for x in data)) / (number_total_elements - 1)
 
+hmu.print_centered("variance_of_sample",80,"*")
 print(variance_of_sample)
 
-print("standard_deviation")
+hmu.print_centered("standard_deviation",80,"*")
 standard_deviation = variance_of_sample ** 0.5
 print(standard_deviation)
 
-print("variance_coefficient")
+hmu.print_centered("variance_coefficient",80,"*")
 variance_coefficient = standard_deviation/average
 print(variance_coefficient)
