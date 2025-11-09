08265c68e7de57957f90a0ba1cdc77e704cc5e0d
Gabriel Ángel Zárate Domínguez <gabriel.zarate5909@alumnos.udg.mx>
2025-11-08 20:48:05 -0600

    * Refactoring dispersion_measurements

--- max_min_normal.py
+++ max_min_normal.py
@@ -19,28 +19,28 @@ agustin_min_proportion_low_value : float = average - standard_deviation
 agustin_max_proportion_low_value : float = average + standard_deviation
 agustin_lowZ_first_form: float = (agustin_min_proportion_low_value - average)/standard_deviation
 agustin_highZ_first_form: float = (agustin_max_proportion_low_value - average)/standard_deviation
-agustin_min_proportion : float = 1 - ((1/2) ** standard_deviation)
-agustin_min_proportion : float = 1 - ((1/2) ** standard_deviation)
+agustin_min_proportion : float = 1 - ((1/2) ** agustin_highZ_first_form)
 
 print("Statistical Summary")
 print("-" * 40)
-print(f"Minimum value in array: {global_min_value:.2f}")
-print(f"Maximum value in array: {global_max_value:.2f}")
+print(f"Minimum global value in array: {global_min_value:.2f}")
+print(f"Maximum global value in array: {global_max_value:.2f}")
 print(f"Average value: {average:.2f}")
 print(f"Standard deviation: {standard_deviation:.2f}")
 print()
 
 print("Agustin's Proportion Details")
 print("-" * 40)
-print(f"Min proportion range: {agustin_lowZ_first_form:.2f} to {agustin_highZ_first_form:.2f}")
+print(f"Min proportion range values: {agustin_min_proportion_low_value:.2f} to {agustin_max_proportion_low_value:.2f}")
+print(f"Min proportion range z values: {agustin_lowZ_first_form:.2f} to {agustin_highZ_first_form:.2f}")
 print(f"Agustin minimum proportion: {agustin_min_proportion:.2f}")
 
-p = 0.7
+p = 0.65
 log_term = math.log(1 / (1 - p)) / math.log(2)
 low_range = average - (standard_deviation * log_term)
 high_range = average + (standard_deviation * log_term)
 
-print(f"Agustin proportion at 70%: {low_range:.2f} to {high_range:.2f}")
+print(f"Agustin proportion at 65%: {low_range:.2f} to {high_range:.2f}")
 # Curva normal teórica
 x = np.linspace(global_min_value, global_max_value, 100)
 y = norm.pdf(x, average, standard_deviation)
